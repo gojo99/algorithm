c8b952b26dcf1edfbf5277ffe74734840eff8bc9
Taranpreet Singh <97303226+gojo99@users.noreply.github.com>
2022-03-04 09:23:01 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,55 @@
 import numpy as np
 import math
+from tkinter import *
+# creates the window
+root = Tk()
+# names the window
+root.wm_title("Simplex")
 
-function = [int(n) for n in input('Enter coefficients of the function and add 0s for '
-                                  'each slack \ surplus ''variables: ').split()]
-function2 = np.array(function)
-np.savetxt('function.txt', function2, fmt='%d')
-ca = np.loadtxt('function.txt', dtype=int)
+ca = np.loadtxt('D:\\function.txt', dtype=int)
 inter = np.multiply(ca, -1)
 c = inter.tolist()
-c2 = c + [0]
 
-r = int(input('How many constraints do you have: '))
-# initializing an empty matrix
-arr = []
-# taking 2x2 matrix from the user
-for i in range(r):
-    # taking row input from the user
-    row = list(map(int, input('Enter the constraints and add +1 for slack and -1 for surplus variables'
-                              ' at the correct positions: ').split()))
-    # appending the 'row' to the 'matrix'
-    arr.append(row)
-
-arr2 = np.array(arr)
-np.savetxt('constraints.txt', arr2, fmt='%d')
-inter2 = np.loadtxt('constraints.txt', dtype=int)
+inter2 = np.loadtxt('D:\\constraints.txt', dtype=int)
 a = inter2.tolist()
 
-brrr = [int(n) for n in input('Enter the value for each constraint inequality: ').split()]
-arr3 = np.array(brrr)
-np.savetxt('constraints_values.txt', arr3, fmt='%d')
-inter3 = np.loadtxt('constraints_values.txt', dtype=int)
+inter3 = np.loadtxt('D:\\values.txt', dtype=int)
 b = inter3.tolist()
 
+def import_files():
+    return
 
-# create the tableau
-def make_tableau(c2, a, b):
+
+def make_tableau(c, a, b):
     xb = [t + [x] for t, x in zip(a, b)]
     z = c + [0]
     return xb + [z]
 
 
 def can_be_improved(tableau):
+    # finds the final row of the tableau
     z = tableau[-1]
+    # returns true if any of the values in z are negative, but it excludes the final value.
     can_be = any(x < 0 for x in z[:-1])
+    # if true is returned then the optimal solution has not been found, and we continue to perform the pivot operations
     return can_be
 
 
 def find_pivot_position(tableau):
+    # z is assigned the last row of the tableau
     z = tableau[-1]
+    # this gives the index of the lowest value in the list
     column = np.argmin(z)
 
     arr = []
+    # runs through all rows except the last row
     for t in tableau[:-1]:
         el = t[column]
-        arr.append(math.inf if el <= 0 else t[-1] / el)
-
-    row = arr.index(min(arr))
+        # el_array = np.array(el)
+        arr.append(math.inf if el <= 0 else np.array(t[-1]) / np.array(el))
+    arr2 = np.array(arr)
+    # row = arr.index(min(arr))
+    row = np.argmin(arr2)
     return row, column
 
 
@@ -92,8 +86,8 @@ def get_solution(tableau):
     return solutions
 
 
-def simplex(c2, a, b):
-    tableau = make_tableau(c2, a, b)
+def simplex(c, a, b):
+    tableau = make_tableau(c, a, b)
 
     while can_be_improved(tableau):
         pivot_position = find_pivot_position(tableau)
@@ -102,12 +96,27 @@ def simplex(c2, a, b):
     return get_solution(tableau)
 
 
-simplex(c2, a, b)
-solution = simplex(c2, a, b)
-real_solution = np.array(solution, dtype='int')
-resu = real_solution*c2*(-1)
-final_resu = np.sum(resu)
+solution = simplex(c, a, b)
+solution2 = np.round_(solution, decimals = 0)
+real_solution = np.array(solution2, dtype='int')
+# resu = real_solution*c2*(-1)
+# final_resu = np.sum(resu)
+# print('solution: ', real_solution)
 
 
-print('solution: ', real_solution)
-print('Profit: ', final_resu)
+def click():
+    label1 = Label(root, text=real_solution)
+    label1.pack()
+
+
+# button1 = Button(root, text='Import files', command=import_files)
+# button2 = Button(root, text='Solve', command=lambda: [make_tableau, can_be_improved, find_pivot_position,
+#                                                      pivot_step, is_basic, get_solution, simplex, click])
+button3 = Button(root, text='Display results', command=click)
+
+
+# button1.pack()
+# button2.pack()
+button3.pack()
+
+root.mainloop()
